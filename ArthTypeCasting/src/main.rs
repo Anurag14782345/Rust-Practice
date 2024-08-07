448972e17965357f2eba2bf5bb7d0e66c1386aa1
annuraagggiiit <anuragkhanna@iiitmanipur.ac.in>
2024-08-07 15:04:14 +0530

    Arthematic adn type Casting

--- ArthTypeCasting/src/main.rs
+++ ArthTypeCasting/src/main.rs
@@ -1,8 +1,16 @@
+use std::io;
 fn main() {
     let x=9 as i64;
     let y= 10 as i16;
     
     let z= x+(y as i64) ; 
     println!("{}",z);
+    start();
+}
+fn start() {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Fail");
+    let no: i64=input.trim().parse().unwrap();
+    println!("{}", no + 2);
 }
  
